ed250553a7d20cffe79566116a80939ce16b2289
kb0rg <kristenmborges@gmail.com>
2015-02-23 12:08:16 -0800

    first commit. rough scaffold in place, data model not yet in place.

---
+++ surfjounal.py
@@ -0,0 +1,98 @@
+from flask import Flask, request, session, render_template, g, redirect, url_for, flash
+import model
+import jinja2
+import os
+
+app = Flask(__name__)
+app.secret_key = '\xf5!\x07!qj\xa4\x08\xc6\xf8\n\x8a\x95m\xe2\x04g\xbb\x98|U\xa2f\x03'
+app.jinja_env.undefined = jinja2.StrictUndefined
+
+@app.route("/")
+def index():
+    """This is the 'cover' page of the ubermelon site"""
+    return render_template("index.html")
+
+@app.route("/sessionsSummary")
+def list_sessionsInfo():
+    """list of all of the potential info that can be collected"""
+    # melons = model.get_melons()
+    # return render_template("surf_sessions.html",
+    #                        session_list = sessions)
+    return render_template("surf_sessions_summary.html")
+
+@app.route("/sessions")
+def list_sessions():
+    """diplay all of the surf sessions logged so far"""
+    # melons = model.get_melons()
+    # return render_template("surf_sessions.html",
+    #                        session_list = sessions)
+    return render_template("surf_sessions_list.html")
+
+
+@app.route("/addSession")
+def add_session():
+    """put everything from the form into the db"""
+    # melons = model.get_melons()
+    # return render_template("surf_sessions.html",
+    #                        session_list = sessions)
+    return render_template("surf_session_add.html")
+
+
+@app.route("/board_quiver")
+def edit_quiver():
+    """display and edit existing quiver of boards"""
+    # melons = model.get_melons()
+    # return render_template("surf_sessions.html",
+    #                        session_list = sessions)
+    return render_template("board_quiver.html")    
+
+    # @app.route("/melon/<int:id>")
+# def show_melon(id):
+#     """This page shows the details of a given melon, as well as giving an
+#     option to buy the melon."""
+#     melon = model.get_melon_by_id(id)
+#     print melon
+#     return render_template("melon_details.html",
+#                   display_melon = melon)
+
+# @app.route("/cart")
+# def shopping_cart():
+#     """TODO: Display the contents of the shopping cart. The shopping cart is a
+#     list held in the session that contains all the melons to be added. Check
+#     accompanying screenshots for details."""
+#     return render_template("cart.html")
+
+# @app.route("/add_to_cart/<int:id>")
+# def add_to_cart(id):
+#     """TODO: Finish shopping cart functionality using session variables to hold
+#     cart list.
+
+#     Intended behavior: when a melon is added to a cart, redirect them to the
+#     shopping cart page, while displaying the message
+#     "Successfully added to cart" """
+
+#     return "Oops! This needs to be implemented!"
+
+
+@app.route("/login", methods=["GET"])
+def show_login():
+    return render_template("login.html")
+
+
+@app.route("/login", methods=["POST"])
+def process_login():
+    """TODO: Receive the user's login credentials located in the 'request.form'
+    dictionary, look up the user, and store them in the session."""
+    return "Oops! This needs to be implemented"
+
+
+# @app.route("/checkout")
+# def checkout():
+#     """TODO: Implement a payment system. For now, just return them to the main
+#     melon listing page."""
+#     flash("Sorry! Checkout will be implemented in a future version of ubermelon.")
+#     return redirect("/melons")
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, port=port)
